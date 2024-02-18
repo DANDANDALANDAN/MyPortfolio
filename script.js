1314997bccba034096d00b651a5fa7b1397d9164
DANDANDALANDAN <sulitdanny1995@gmail.com>
2024-02-18 15:52:26 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+/* Added JS for the FAQs */
+const toggles = document.querySelectorAll('.faq-toggle')
+
+toggles.forEach(toggle => {
+    toggle.addEventListener('click', () => {
+        toggle.parentNode.classList.toggle('active')
+    })
+})
+/*  */
